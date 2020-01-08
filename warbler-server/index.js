80f12d49258e42269144f18ae611fddcbf1f72a6
Karolina Gauvain <karolina.gauvain@gmail.com>
2020-01-08 17:03:17 +0100

    Update index.js

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,19 +1,48 @@
-import express from "express";
+require("dotenv").config();
+const express = require("express");
 const app = express();
-import cors from "cors";
-import { json } from "body-parser";
-
-const PORT = 8081;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
+const db = require("./models");
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
-app.use(function(req, res, next){
+app.use("/api/auth", authRoutes);
+app.use(
+  "/api/users/:id/messages",
+  loginRequired,
+  ensureCorrectUser,
+  messagesRoutes
+);
+
+app.get("/api/messages", loginRequired, async function(req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt: "desc" })
+      .populate("user", {
+        username: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.use(function(req, res, next) {
   let err = new Error("Not Found");
   err.status = 404;
   next(err);
-})
+});
 
-app.listen(PORT, function(){
+app.use(errorHandler);
+
+app.listen(PORT, function() {
   console.log(`Server is starting on port ${PORT}`);
-})
+});
